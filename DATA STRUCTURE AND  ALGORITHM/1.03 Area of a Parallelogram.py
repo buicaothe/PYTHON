c5a93913da69e436f4a3879869e68b5c08790673
buicaothe <buicaothe.pmp@gmail.com>
2026-09-01 15:15:03 +0300

    2026.09.01 FInal commit

---
+++ DATA STRUCTURE AND  ALGORITHM/1.03 Area of a Parallelogram.py
@@ -0,0 +1,15 @@
+# 1.03 Area of a Parallelogram:
+
+while True:
+    try:
+        b = int(input('Enter the base of the Parallelogram, a = '))
+        h = int(input('Enter the height of the Parallelogram, h = '))
+        if b*h > 0:
+            break
+        else:
+            print('The number should be positive! Please re-type:')
+    except ValueError:
+        print('Not a NUMBER!!!!!')
+
+area = b*h
+print(f'The Area of a Parallelogram base {b} and height {h} is {area}.')
