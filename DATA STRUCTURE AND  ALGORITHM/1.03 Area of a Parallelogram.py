aff07db21b3dc3f93897b501d6bd5b4bfb19454f
buicaothe <buicaothe.pmp@gmail.com>
2026-09-01 15:28:17 +0300

    2026.09.01 Correction to the positive input

--- DATA STRUCTURE AND  ALGORITHM/1.03 Area of a Parallelogram.py
+++ DATA STRUCTURE AND  ALGORITHM/1.03 Area of a Parallelogram.py
@@ -4,10 +4,10 @@ while True:
     try:
         b = float(input('Enter the base of the Parallelogram, b = '))
         h = float(input('Enter the height of the Parallelogram, h = '))
-        if b*h > 0:
+        if (b > 0) and (h > 0):
             break
         else:
-            print('The number should be positive! Please re-type:')
+            print('The numbers should be positive! Please re-type:')
     except ValueError:
         print('Not a NUMBER!!!!!')
 
