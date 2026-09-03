ef3d018060c21c532f6e9cb4fce2dfb12ab94a1b
buicaothe <buicaothe.pmp@gmail.com>
2026-09-03 19:47:18 +0300

    2026.09.03 First commit

---
+++ APPLIED MATHEMATICS/01. Number Sequence.py
@@ -0,0 +1,12 @@
+# IMPORTING THE PACKAGES
+# THIS CELL NEEDS TO BE EXECUTED BEFORE THEY CAN BE USED
+import math
+import numpy as np
+import scipy as sp
+import sympy as sy
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+import hashlib
+import cryptography
+# %matplotlib inline
