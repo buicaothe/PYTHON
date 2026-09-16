7260cd7f618e3b6b30ddecff07db87de85e4c240
buicaothe <buicaothe.pmp@gmail.com>
2026-09-16 12:16:19 +0300

    2026.01.16 Pandas_Basics

--- DATA STRUCTURE AND  ALGORITHM/Recursion/04 Sum of any Sequence.py
+++ DATA STRUCTURE AND  ALGORITHM/Recursion/04 Sum of any Sequence.py
@@ -1,14 +1,15 @@
 # Iterative:
 def sum1(n):
     an=0
-    for i in range(1,n+1):
+    for i in range(1,n+1,2):
         an = an+i
         print(i,'+')
     return an
-print('Iterative :',sum1(5))
+print('Iterative :',sum1(100))
 
 # Recursive:
 def sum2(n):
-    if i>n:
-        return n
-    return sum2(n,i+1)
+    if n <=1:
+        return 1
+    return sum2(n-2)+n
+print('Recrusive :',sum2(100))
